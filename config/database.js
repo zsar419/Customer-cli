f15d811bb837edcc2f73534573fa69d36af04cdb
zsar419 <zsar419@aucklanduni.ac.nz>
2017-11-19 16:21:05 +1300

    db strings ignored

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-    module.exports = { mongoURI: '< your mlab db url >' }  // DB for production mlab (heroku deployment)
+    module.exports = { mongoURI: 'mongodb://zeta:zeta@ds113606.mlab.com:13606/customer-cms' }  // DB for production mlab (heroku deployment)
 } else {
-    module.exports = { mongoURI: '< your local mongodb url >' }                      // DB for local deployment
+    module.exports = { mongoURI: 'mongodb://localhost:27017/customercli' }                      // DB for local deployment
 }
