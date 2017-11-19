02fec0df54369162fe915247b8cef77fed6a584f
Z <impossible713@hotmail.com>
2017-11-19 16:22:27 +1300

    Update database.js
    removed public connection strings

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-    module.exports = { mongoURI: 'mongodb://zeta:zeta@ds113606.mlab.com:13606/customer-cms' }  // DB for production mlab (heroku deployment)
+    module.exports = { mongoURI: '< your mlab db connection >' }  // DB for production mlab (heroku deployment)
 } else {
-    module.exports = { mongoURI: 'mongodb://localhost:27017/customercli' }                      // DB for local deployment
+    module.exports = { mongoURI: '< your mongo db local connection >' }                      // DB for local deployment
 }
